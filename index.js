3abf5b3c0de7b38cd8daf7a4eb5a054c5d01c9d5
irene29-hub <muemarenee@gmail.com>
2025-06-24 15:59:02 +0300

    add changes

--- index.js
+++ index.js
@@ -3,17 +3,5 @@ function displayMessage(message) {
     messageElement.textContent = message;
     // console.log(message);
 }
-fetch("http://localhost:3000/posts")
-    .then(response => response.json())
-    .then(data => {
-        if (data && data.length > 0) {
-            displayMessage(`Fetched ${data.length} posts successfully.`);
-        } else {
-            displayMessage("No posts found.");
-        }
-    })
-    .catch(error => {
-        console.error("Error fetching posts:", error);
-        displayMessage("Failed to fetch posts.");
-    }); 
-// Ensure the DOM is fully loaded before running the script
+fetch("https://dummyjson.com/products")
+    

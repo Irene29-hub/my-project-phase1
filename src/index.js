111df1c9c7b919054f3ac2178451998949e3ae24
irene29-hub <muemarenee@gmail.com>
2025-06-26 19:35:10 +0300

    add changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-function displayMessage(message) {
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = message;
-    // console.log(message);
+document.addEventListener('DOMContentLoaded', fetchProducts);
+
+function fetchProducts() {
+    const apiURL = 'https://dummyjson.com/products';  
+    const container = document.getElementById('productsContainer');
+
+    fetch(apiURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch products');
+            }
+            return response.json();
+        })
+        .then(products => {
+            container.innerHTML = '';
+            products.forEach(product => {
+                const productCard = document.createElement('div');
+                productCard.className = 'product-card';
+                productCard.innerHTML = `
+                    <h3>${product.name}</h3>
+                    <p>Price:$${product.price}</p>
+                `;
+                container.appendChild(productCard);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
-fetch("https://dummyjson.com/products")
-    
